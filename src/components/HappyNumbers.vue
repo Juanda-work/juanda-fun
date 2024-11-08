6ed96649380eebcd389c6a017449fcf0310bdee2
Juan David Work <juan_david_osorio_work@outlook.es>
2024-11-08 22:41:27 +0100

    added counter

--- src/components/HappyNumbers.vue
+++ src/components/HappyNumbers.vue
@@ -28,8 +28,8 @@
                         @click="checkNumber">
                         Comprobar
                     </v-btn>
-                    <v-spacer class="my-5"></v-spacer>
                     <v-btn
+                        class="ml-5"
                         elevation="2"
                         outlined
                         color="red"
@@ -44,20 +44,28 @@
                         @click="randomNumber">
                         Aleatorio
                     </v-btn>
+                    <v-btn
+                        class="ml-5"
+                        elevation="2"
+                        outlined
+                        color="gray"
+                        @click="checkAttemps">
+                        Ver intentos
+                    </v-btn>
                 </v-col>
 
                 <v-col cols="3" class="finalMessage">
                     <div> 
-                        <v-fab-transition v-if="isHappy"> 
-                            <v-alert v-if="showHappyMsg()" type="success"> El número es feliz </v-alert> 
+                        <v-fab-transition v-show="isHappy"> 
+                            <v-alert v-show="showHappyMsg()" type="success"> El número es feliz </v-alert> 
                         </v-fab-transition> 
-                        <v-fade-transition v-if="!isHappy"> 
-                            <v-alert v-if="showUnhappyMsg()" type="error"> No es feliz... </v-alert> 
+                        <v-fade-transition v-show="!isHappy"> 
+                            <v-alert v-show="showUnhappyMsg()" type="error"> No es feliz... </v-alert> 
                         </v-fade-transition> 
                     </div>
                 </v-col>
 
-                <v-col cols="5" class="mt-5">
+                <v-col cols="5">
                     <v-card-text>
                         <div class="font-weight-bold ml-8 mb-2">
                             <h2>Resultados:</h2>
@@ -84,6 +92,46 @@
                     </v-card-text>
                 </v-col>
             </v-row>
+
+            <v-dialog
+                v-model="showAttemps"
+                width="500"
+                >
+                <v-card>
+                    <v-card-title class="text-h4 grey lighten-2">
+                     Intentos
+                    </v-card-title>
+
+                    <v-card-text>
+                        <div class="title">
+                            <span color="green"> Acertados: {{ attempsOk }} </span>
+                        </div>
+                        <div class="title">
+                            <span color="red"> Fallados: {{ attempsFailed }} </span>
+                        </div>
+                    </v-card-text>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                        <v-btn
+                            color="red"
+                            text
+                            @click="resetAttemps"
+                        >
+                            Resetear intentos
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                            color="primary"
+                            text
+                            @click="showAttemps = false"
+                        >
+                            Cerrar
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-container>
     </v-app>
 </template>
@@ -102,6 +150,9 @@ export default {
             iterationNumbers : [],
             isHappy : false,
             valid: false,
+            attempsOk : 0,
+            attempsFailed : 0,
+            showAttemps : false,
         }
     },
     methods: {
@@ -145,10 +196,12 @@ export default {
                     results.push(aux);
                     this.numberRepeated = aux;
                     this.iterationQuantity = results.length;
+                    this.attempsFailed++;
                     return results;
                 }else if( aux == 1 ){
                     results.push(aux);
                     this.iterationQuantity = results.length;
+                    this.attempsOk++;
                     this.isHappy = true;
                     return results;
                 }
@@ -183,7 +236,14 @@ export default {
         },
         showUnhappyMsg(){
             return !this.isHappy && this.iterationQuantity && this.numberToSearch;
-        }
+        },
+        checkAttemps(){
+            this.showAttemps = true;
+        },
+        resetAttemps(){
+            this.attempsOk = 0;
+            this.attempsFailed = 0;
+        },
     },
     watch:{
         number(value){
@@ -197,5 +257,11 @@ export default {
 <style scoped>
     .finalMessage{
         margin-top: 50px;
+    }
+    .green{
+        color: green;
+    }
+    .red{
+        color: red;
     }
 </style>
